6b1012db24fbed90178a250f2e06bf436d49e39d
Abdelhakim Nassif <anassif@e1r5p6.1337.ma>
2020-11-26 20:55:37 +0100

    26-11-21h

--- libasm?/ft_write.s
+++ libasm?/ft_write.s
@@ -2,6 +2,6 @@ section .text
     global _ft_write
 
 _ft_write:
-    mov rax, 1
+    mov rax, 0x2000004
     syscall
     ret
--- libasm?/main.c
+++ libasm?/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int     hello();
 size_t  ft_strlen(char *s);
 char    *ft_strcpy(char *s1, char *s2);
 int     ft_strcmp(char *s1, char *s2);
 ssize_t ft_write(int filedest, const void *buf, size_t nbyte);
+ssize_t ft_read(int fildes, void *buf, size_t nbyte);
+
 int main()
 {
     char dst[15];
     char *src = "yoyoyoy";
     char *s1 = "";
     char *s2 = "";
-    //printf("%d\n", hello()); 
+    char buff[20];
+    int fd;
+    
     //printf("%d", ft_strlen("123456789"));
     /*ft_strcpy(dst, src);
     printf("<%s>", dst);*/
-    printf("<%d>", ft_strcmp(s1, s2));
-    //ft_write(1, "dadawdad", 8);
+    //printf("<%d>", ft_strcmp(s1, s2));
+    //ft_write(1, (const void *)"dadawdad", 8);
+    fd = open("ft_read.s", O_RDONLY);
+    ft_read(fd, buff, 10);
+    printf("%s", buff);
     //printf("%zd",ft_write(1, (const void *)"dadawdad", 8));
     return (0);
 }
