74ae9bde3a33ec5ad199fbe73773313072e70e0f
Abdelhakim Nassif <anassif@e2r1p8.1337.ma>
2020-12-11 23:13:23 +0100

    11-12-fixederrno

--- ft_strdup.s
+++ ft_strdup.s
@@ -22,5 +22,9 @@ _ft_strdup:
 	ret
 
 _error:
-	mov rax, -1
-	ret
+    push rax        ; save errno f rax
+    call ___error   ; rax now points to external variable errno
+    pop r8          ; pop rax f r8 (r8 fih code d error)
+    mov [rax], r8   ; poiti rax 3la r8 bach dereferenci rax
+    mov rax, -1     ; return -1 (za3ma kayn error)
+    ret
